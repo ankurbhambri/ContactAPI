e6d1710cc2aadaa6465aa81ffd9f01e2d8ad0307
Ankur Bhambri <ankurwave09@gmail.com>
2022-12-04 14:28:23 +0530

    inital commit

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,87 @@
+﻿using ContactsAPI.Data;
+using ContactsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ContactsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // controller will replace with name Contacts
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbContext dbContext;
+
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet] // for swagger we are adding this anotation else no need
+        public async Task<IActionResult> GetContacts()
+        {
+            return Ok(await dbContext.Contacts.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetContact([FromRoute] Guid id)
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+            if (contact != null)
+            {
+                return Ok(contact);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContacts(AddContacts addContacts)
+        {
+            var contact = new Contacts()
+            {
+                Id = Guid.NewGuid(),
+                Address = addContacts.Address,
+                Email = addContacts.Email,
+                FullName = addContacts.FullName,
+                Phone = addContacts.Phone
+            };
+
+            await dbContext.Contacts.AddAsync(contact);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(contact);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateContacts([FromRoute] Guid id, UpdateContacts updateContacts) 
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+            if (contact != null)
+            {   
+                contact.FullName= updateContacts.FullName;
+                contact.Address= updateContacts.Address;
+                contact.Phone= updateContacts.Phone;
+                contact.Email= updateContacts.Email;
+                await dbContext.SaveChangesAsync();
+                return Ok(contact);
+            }
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteContact([FromRoute] Guid id)
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+            if (contact != null)
+            {
+                dbContext.Remove(contact);
+                await dbContext.SaveChangesAsync();
+                return Ok(contact);
+            }
+            return NotFound();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using ContactsAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// builder.Services.AddDbContext<ContactsAPIDbContext>(op => op.UseInMemoryDatabase("ContactsDb"));
+builder.Services.AddDbContext<ContactsAPIDbContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("DB")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
